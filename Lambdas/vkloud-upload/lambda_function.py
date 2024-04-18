87e649f088b5d2cfa61d5ac2fa2fa481b1136774
iCanDerpU <78787079+iCanDerpU@users.noreply.github.com>
2024-04-18 15:34:38 +0300

    revert lambda changes

--- Lambdas/vkloud-upload/lambda_function.py
+++ Lambdas/vkloud-upload/lambda_function.py
@@ -4,7 +4,7 @@ import os
 from datetime import datetime
 
 def lambda_handler(event, context):
-    base64_image = event['body']['file']
+    base64_image = event['file']
     image_bytes = base64.b64decode(base64_image)
     current_time = datetime.now().timestamp()
     file_name = f"uploads/{current_time}.png"
@@ -14,17 +14,11 @@ def lambda_handler(event, context):
         s3.put_object(Bucket=s3_bucket_name, Key=file_name, Body=image_bytes)
         return {
             'statusCode': 200,
-            'headers': {
-                'Access-Control-Allow-Origin': '*',
-            },
             'body': f"Successfully uploaded {file_name} to {s3_bucket_name}"
         }
     except Exception as e:
         print(e)
         return {
             'statusCode': 500,
-            'headers': {
-                'Access-Control-Allow-Origin': '*',
-            },
             'body': "Error uploading the image"
         }
